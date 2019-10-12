ad6e3d7dd4621c71fb42f24f4bc4b4c36eae6568
Nelias <michal.antropik@gmail.com>
2019-10-12 14:36:13 +0200

    feat: add vscode settings

--- math-library/math.cpp
+++ math-library/math.cpp
@@ -3,13 +3,13 @@
 
 auto first_number = 5;
 
-int main() 
+int main()
 {
     std::cout << "The power of 6 to 6 is: " << pow(6, 6) << std::endl;
 
     std::cout << "Type a number: ";
     std::cin >> first_number;
     std::cout << "Your number is: " << first_number << std::endl;
-    
+
     return 0;
 }
