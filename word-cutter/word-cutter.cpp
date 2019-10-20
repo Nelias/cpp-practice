ca97bdffae4f7cda739ac4cbbb197c8340354788
Nelias <michal.antropik@gmail.com>
2019-10-20 16:39:16 +0200

    feat(word-cutter): add shrink_to_fit function

--- word-cutter/word-cutter.cpp
+++ word-cutter/word-cutter.cpp
@@ -6,6 +6,7 @@ int main()
     std::string word;
     std::cout << "What word do you want to cut into pieces?" << std::endl;
     getline(std::cin, word);
+    word.shrink_to_fit();
     std::cout << "You picked: " << word << std::endl;
 
     for (int i = 0; i < word.length(); i++)
