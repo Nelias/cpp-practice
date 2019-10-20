be4c63b97b91196d7ca544c39f53bd084737a9a0
Nelias <michal.antropik@gmail.com>
2019-10-20 17:12:41 +0200

    feat: add a program that swaps two strings

---
+++ word-cutter/swap.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string string_one = "a nice name";
+
+    std::string string_two = "even better name";
+
+    char ch[80];
+
+    string_one.copy(ch, 13, 0);
+
+    std::cout << "The new copied character array is: ";
+    std::cout << ch << std::endl
+              << std::endl;
+
+    std::cout << "The 1st string before swapping is: ";
+    std::cout << string_one << std::endl;
+    std::cout << "The 2nd string before swapping is: ";
+    std::cout << string_two << std::endl;
+
+    string_one.swap(string_two);
+
+    std::cout << "The 1st string after swapping is: ";
+    std::cout << string_one << std::endl;
+    std::cout << "The 2nd string after swapping is: ";
+    std::cout << string_two << std::endl;
+
+    return 0;
+}
