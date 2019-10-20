dc624165dfb0a61baf57837e2616a127d98b3f34
Nelias <michal.antropik@gmail.com>
2019-10-20 16:10:38 +0200

    feat(word-cutter): add a new line after the word is cut

--- word-cutter/word-cutter.cpp
+++ word-cutter/word-cutter.cpp
@@ -2,13 +2,18 @@
 
 int main()
 {
-    char word[50];
+    char word[15];
     std::cout << "What word do you want to cut into pieces?" << std::endl;
     std::cin >> word;
     std::cout << "You picked: " << word << std::endl;
-    for (int i = 0; i < 50; i++)
+
+    for (int i = 0; i < 15; i++)
     {
-        std::cout << word[i] << std::endl;
+        std::cout << word[i] << " ";
+        if (i == 15)
+        {
+            std::cout << "\n";
+        }
     }
     return 0;
 }
