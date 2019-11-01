9592276b239f3125f6e267c5ddf220c2f4c7262c
Nelias <michal.antropik@gmail.com>
2019-11-01 21:22:35 +0100

    feat: add two more variables to a class

--- classes/class.cpp
+++ classes/class.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <string>
 
 class MyNumber
 {
 public:
     int my_number;
+    std::string nameInitial;
+    std::string surnameInitial;
 };
 
 int main()
@@ -11,8 +14,12 @@ int main()
     MyNumber my_object;
 
     my_object.my_number = 15;
+    my_object.nameInitial = "J";
+    my_object.surnameInitial = "K";
 
     std::cout << my_object.my_number << std::endl;
+    std::cout << my_object.nameInitial << std::endl;
+    std::cout << my_object.surnameInitial << std::endl;
 
     return 0;
 }
