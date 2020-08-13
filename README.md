3e2f9c59af4f88c3d05914f5d16922cf2e85abbb
Nelias <michal.antropik@gmail.com>
2020-08-13 22:28:48 +0200

    implement a new version of a snake game based solely on a standard library

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # cpp-practice
+
 C++ programs written to practice this language
 
 ## Running a program
@@ -6,3 +7,7 @@ C++ programs written to practice this language
 1. In your terminal window from the root folder go to a program's folder `cd folder-name`
 2. Run `clang++ program-name.cpp`
 3. Run `./a.out`
+
+## Tips
+
+Do not use `<conio.h>` because it is non-standard and it is non-portable.
--- snake-game/snake-game.cpp
+++ snake-game/snake-game.cpp
@@ -1,51 +1,124 @@
 #include <iostream>
-#include <vector>
 #include <ctime>
+#include <string>
+#include <chrono>
+using namespace std::chrono;
 
-class Snake
-{
-private:
-    int snake_head_position, random_number, dollars, tail_coordinates, bonus, time, tail;
-    char *map;
-    bool L, R, D, U, A; // L = LEFT, R = RIGHT, D = DOWN, U = UP, A = A BOOLEAN
-    std::vector <int> past; // A special vector for deleting old parts
-
-public:
-    Snake()
-    {
-        map = new char [2000];
-        L = 1;
-        R = 0;
-        D = 0;
-        U = 0;
-        A = 0;
-        tail = 1; // Actually it will not have a tail until it eats the first apple
-        snake_head_position = 1000; // Position of a snake at the beggining
-        dollars = 0; // Money
-        bonus = 0;
-        time = 40; // For bonus apples
-        random_number = 1; // So first apple generation will not be bugged
-	}
-
-    ~Snake()
-    {
-        delete [] map;
-    }
-
-    void graphics();
-    void gameLogic();
-    void KEYBOARD();
-    void apple();
-    int gameOver();
-    void start();
-    friend void clearScreen();
-    friend void sp(); // Choose a color function
-    friend void s(); // Forget color choice
-};
+#define KEY_UP 65
+#define KEY_DOWN 66
+#define KEY_RIGHT 67
+#define KEY_LEFT 68
 
 int main()
 {
-    srand((unsigned)time(0)); // Random numbers will always be random
-    Snake SNAKE;
-    SNAKE.start();
+    const int height = 10;
+    const int width = 20;
+    std::string map [height][width];
+
+    bool is_game_over = false;
+    int snake_y = 2;
+    int snake_x = 7;
+    int pressed_key;
+    int apples_collected = 0;
+    int apples_needed = 8;
+    int apple_x = (rand() % width - 2 + 1) + 2;
+    int apple_y = 2 + (rand() % static_cast<int>(8 - 2 + 1));
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++) 
+        {
+
+            if (row == 0 || row == height - 1) 
+            {
+                map[row][column] = "...";
+            } else if (column == apple_x && row == apple_y) 
+            {
+                map[row][column] = " ó ";
+            } else 
+            {
+                map[row][column] = "   ";
+            }
+
+        }
+    }
+
+    while (is_game_over != true)
+    {
+        system("clear");
+
+        if (snake_y == 0 || snake_y == height - 1 || snake_x == width || snake_x == -1) {
+            is_game_over = true;
+            std::cout << "\n\nYou ran away! Game Over!\n" << std::endl;
+            break;
+        }
+
+
+        for (int row = 0; row < height; row++)
+        {
+
+            for (int column = 0; column < width; column++) 
+            {
+
+                if (column == apple_x && row == apple_y && column != snake_x && row != snake_y) 
+                {
+                    map[row][column] = " ó ";
+                }
+
+                std::cout << map[row][column];
+            }
+
+            std::cout << "\n";
+        }
+
+        if (apples_collected == apples_needed) {
+            is_game_over = true;
+            std::cout << "\n\nYou win! All " << apples_collected << " apples have been collected!\n" << std::endl;
+            break;
+        }
+
+        if (snake_x == apple_x && snake_y == apple_y) {
+            apples_collected++;
+            apple_x = (rand() % width - 1) + 1;
+            apple_y = 2 + (rand() % static_cast<int>(8 - 2 + 1));
+        }
+        
+        system("stty raw");
+    
+        pressed_key = getchar();
+
+
+        if (pressed_key == KEY_RIGHT) 
+        {
+            snake_x++;
+            map[snake_y][snake_x - 1] = "   ";
+        }
+
+        if (pressed_key == KEY_LEFT) 
+        {
+            snake_x--;
+            map[snake_y][snake_x + 1] = "   ";
+        }
+
+        if (pressed_key == KEY_DOWN) 
+        {
+            snake_y++;
+            map[snake_y - 1][snake_x] = "   ";
+        }
+
+        if (pressed_key == KEY_UP) 
+        {
+            snake_y--;
+            map[snake_y + 1][snake_x] = "   ";
+        }
+
+
+        map[snake_y][snake_x] = " ༼ ";
+
+        system("stty cooked");
+    };
+
+    return 0;
 }
+
+/* run: g++ -std=c++11 snake-game.cpp -o a.out */
